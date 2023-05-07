0fe01ecc2bd72b676c05bcf6931e5830a03510c6
luongvietan <nguyenvantai.231123@gmail.com>
2023-05-07 22:59:59 +0700

    fixed

--- admin.php
+++ admin.php
@@ -1,10 +1,51 @@
+<?php
+session_start();
+
+// Kiểm tra xem người dùng đã đăng nhập và có quyền quản trị hay không
+if (!isset($_SESSION['username']) || $_SESSION['role'] !== 'admin') {
+  header("Location: login.php");
+  exit();
+}
+
+// Kiểm tra xem người dùng đã nhấn nút Approve hay chưa
+if (isset($_POST['approve'])) {
+  $order_id = $_POST['order_id'];
+
+  // Cập nhật trạng thái của đơn hàng trong cơ sở dữ liệu
+  // Ví dụ: UPDATE orders SET status='approved' WHERE id=$order_id;
+  // Sau đó, lưu trạng thái mới vào session để sử dụng trên trang payment_cash.php
+  $_SESSION['order_status'] = 'approved';
+
+  // Chuyển hướng người dùng đến trang order_success.php
+  header("Location: order_success.php");
+  exit();
+}
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Trang Admin</title>
+  <title>Admin - Orders</title>
 </head>
 <body>
-    <h2>Đây là trang admin.</h2>
-    <a href="logout.php">Đăng xuất</a>
+  <h1>Orders</h1>
+  <table>
+    <tr>
+      <th>Order ID</th>
+      <th>Status</th>
+      <th>Action</th>
+    </tr>
+    <tr>
+      <td>12345</td>
+      <td>Pending</td>
+      <td>
+        <form method="post">
+          <input type="hidden" name="order_id" value="12345">
+          <button type="submit" name="approve">Approve</button>
+        </form>
+      </td>
+    </tr>
+    <!-- các hàng khác -->
+  </table>
 </body>
 </html>
--- task_2/payment_cash.php
+++ task_2/payment_cash.php
@@ -1,28 +1,28 @@
 <?php
-// Kết nối cơ sở dữ liệu
-$db = new PDO('mysql:host=localhost;dbname=form2;charset=utf8', 'admin', 'admin');
+session_start();
 
-// Lấy thông tin đơn hàng từ tham số URL
-$order_id = $_GET['order_id'];
-$order = $db->prepare('SELECT * FROM orders WHERE order_id = ?');
-$order->execute([$order_id]);
-$order = $order->fetch(PDO::FETCH_ASSOC);
-
-// Nếu không tìm thấy đơn hàng, chuyển hướng về trang danh sách mặt hàng
-if (!$order) {
-  header('Location: list_items.php');
+// Kiểm tra xem người dùng đã đăng nhập chưa
+if (!isset($_SESSION['username'])) {
+  header("Location: login.php");
   exit();
 }
 
-// Cập nhật trạng thái đơn hàng và giảm số lượng hàng trong kho
-$db->beginTransaction();
-$db->exec('UPDATE orders SET order_status = "paid" WHERE order_id = ' . $order_id);
-$db->exec('UPDATE category SET Category_Quantity = Category_Quantity - ' . $order['quantity'] . ' WHERE Category_ID = ' . $order['order_id']);
-$db->commit();
+// Kiểm tra xem đơn hàng đã được duyệt hay chưa
+$status = isset($_SESSION['order_status']) ? $_SESSION['order_status'] : "pending";
+if ($status === "approved") {
+  header("Location: order_success.php");
+  exit();
+}
+
 ?>
 
-<h2>Payment successful</h2>
-
-<p>Your order has been paid in cash. Thank you for your purchase!</p>
-
-<p><a href="order_status.php?order_id=<?php echo $order_id; ?>">Check order status</a></p>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Payment - Cash</title>
+</head>
+<body>
+  <h1>Payment - Cash</h1>
+  <p>Your order is being processed. Please wait for approval from the administrator.</p>
+</body>
+</html>
