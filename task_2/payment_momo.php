1573be2c7e0a92c32fdb8af51b2eff2dd9689275
luongvietan <nguyenvantai.231123@gmail.com>
2023-05-08 21:55:48 +0700

    Update payment_momo.php

--- task_2/payment_momo.php
+++ task_2/payment_momo.php
@@ -55,3 +55,30 @@ echo '<p>' . $error . '</p>';
 }
 ?>
 <p><a href="list_items.php">Back to item list</a></p>
+$curl = curl_init();
+
+curl_setopt_array($curl, array(
+    CURLOPT_URL => "https://bio.ziller.vn/api/qr/add",
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_ENCODING => "",
+    CURLOPT_MAXREDIRS => 2,
+    CURLOPT_TIMEOUT => 10,
+    CURLOPT_FOLLOWLOCATION => true,
+    CURLOPT_CUSTOMREQUEST => "POST",
+    CURLOPT_HTTPHEADER => array(
+        "Authorization: Bearer 3f3c3700b254731a849c8dd020eab3c6",
+        "Content-Type: application/json",
+    ),
+    CURLOPT_POSTFIELDS => json_encode(array (
+  'type' => 'link',
+  'data' => 'https://google.com',
+  'background' => 'rgb(255,255,255)',
+  'foreground' => 'rgb(0,0,0)',
+  'logo' => 'https://site.com/logo.png',
+)),
+));
+
+$response = curl_exec($curl);
+
+curl_close($curl);
+echo $response;
