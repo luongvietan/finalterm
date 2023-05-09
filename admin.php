6015ebc8a328211a792f5adc7265fa2ac63aad6d
luongvietan <nguyenvantai.231123@gmail.com>
2023-05-09 18:03:43 +0700

    lastest

--- admin.php
+++ admin.php
@@ -9,7 +9,7 @@ if (isset($_SESSION['username']) && $_SESSION['username'] === 'admin') {
     echo "<h2>Phê duyệt Đơn hàng</h2>";
     echo '<button onclick="approveOrders()">Phê duyệt Đơn hàng</button>';
     echo "<h2>Xuất kho</h2>";
-    echo '<button onclick="exOrders()">Phê duyệt Đơn hàng</button>';
+    echo '<button onclick="exOrders()">Xuất Đơn hàng</button>';
     // Hiển thị tài khoản người dùng và nút đăng xuất
     echo "<div style='position: absolute; top: 10px; right: 10px;'>";
     echo "Tài khoản : " . $_SESSION['username'] . " ";
@@ -32,7 +32,7 @@ if (isset($_SESSION['username']) && $_SESSION['username'] === 'admin') {
     }
 
     function exOrders() {
-        window.location.href = "/task_3/main3.php";
+        window.location.href = "http://localhost/finalterm/task_3/main3.php";
     }
 </script>
 
