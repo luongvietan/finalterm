700daac02c3b6facbb108a0bba0663cbb0473cdb
luongvietan <nguyenvantai.231123@gmail.com>
2023-05-09 17:56:54 +0700

    fix

--- admin.php
+++ admin.php
@@ -8,6 +8,13 @@ if (isset($_SESSION['username']) && $_SESSION['username'] === 'admin') {
     echo '<button onclick="createReceipt()">Tạo Phiếu nhập kho</button><br><br>';
     echo "<h2>Phê duyệt Đơn hàng</h2>";
     echo '<button onclick="approveOrders()">Phê duyệt Đơn hàng</button>';
+    echo "<h2>Xuất kho</h2>";
+    echo '<button onclick="exOrders()">Phê duyệt Đơn hàng</button>';
+    // Hiển thị tài khoản người dùng và nút đăng xuất
+    echo "<div style='position: absolute; top: 10px; right: 10px;'>";
+    echo "Tài khoản : " . $_SESSION['username'] . " ";
+    echo "<br><a href='logout.php'>Đăng xuất</a>";
+    echo "</div>";
 } else {
     // Chưa đăng nhập, chuyển hướng đến trang đăng nhập
     header('Location: login.php');
@@ -22,6 +29,10 @@ if (isset($_SESSION['username']) && $_SESSION['username'] === 'admin') {
 
     function approveOrders() {
         window.location.href = "approve_orders.php";
+    }
+
+    function exOrders() {
+        window.location.href = "/task_3/main3.php";
     }
 </script>
 
--- task_3/expdf.php
+++ task_3/expdf.php
@@ -28,53 +28,42 @@ $pdf->Cell(30,10,'So Phieu: '.$invoice_number,0,1);
 $pdf->Ln();
 $pdf->Cell(10,10,'STT', 1);
 $pdf->Cell(55,10,'Ten Hang', 1);
-$pdf->Cell(25,10,'Don Gia', 1);
+$pdf->Cell(40,10,'Don Gia (.000 VND)', 1);
 $pdf->Cell(25,10,'So Luong', 1);
 $pdf->Cell(30,10,'Phuong Thuc', 1);
 $pdf->Cell(35,10,'Ma Hang', 1);
 $pdf->Ln();
+
 // Khởi tạo biến lưu trữ thông tin hàng trước đó
 $prev_id = null;
 $prev_row = null;
 
 // Khởi tạo biến tính tổng giá trị cột total
-$ids = array(); // danh sách các id đã xuất hiện
+$total = 0;
+$pdf_rows = array(); // danh sách các hàng đã được gộp
 
 while ($row = mysqli_fetch_assoc($result)) {
     // Kiểm tra xem id của hàng hiện tại đã xuất hiện trong danh sách hay chưa
-    if (in_array($row['item_id'], $ids)) {
-        // Tìm phần tử có cùng id và cộng số lượng và giá trị của hàng hiện tại với phần tử đó
-        foreach ($pdf_rows as &$pdf_row) {
-            if ($pdf_row['item_id'] == $row['item_id']) {
-                $pdf_row['quantity'] += $row['quantity'];
-                //$pdf_row['total'] += $row['total'];
-                break;
-            }
-        }
+    if ($prev_id === $row['item_id'] && $prev_row['payment_method'] === $row['payment_method'] && $prev_row['code'] === $row['code']) {
+        // Cộng số lượng của hàng hiện tại với số lượng của hàng trước đó
+        $prev_row['quantity'] += $row['quantity'];
     } else {
-        // Nếu chưa xuất hiện thì thêm id vào danh sách
-        $ids[] = $row['item_id'];
-        // Xuất thông tin của hàng trước đó (nếu có)
+        // Nếu không giống nhau, thêm hàng trước đó vào danh sách
         if ($prev_row !== null) {
             $pdf_rows[] = $prev_row;
         }
-        // Gán giá trị của hàng hiện tại cho hàng trước đó
+        // Gán giá trị của hàng hiện tại cho hàng trước
         $prev_row = $row;
     }
+
     $prev_id = $row['item_id'];
+}
 
-
-// Xuất thông tin cho hàng cuối cùng (nếu có)
+// Thêm hàng cuối cùng vào danh sách
 if ($prev_row !== null) {
     $pdf_rows[] = $prev_row;
 }
 
-
-// Gán giá trị của hàng hiện tại cho hàng trước đó
-$prev_row = $row;
-$prev_id = $row['item_id'];
-
-}
 // Xuất thông tin các hàng
 $stt = 1;
 $tot = 0;
@@ -84,33 +73,33 @@ foreach ($pdf_rows as $pdf_row) {
 
     $pdf->Cell(10, 10, $stt, 1);
     $pdf->Cell(55, 10, $pdf_row['item_name'], 1);
-    $pdf->Cell(25, 10, $pdf_row['item_price'], 1);
+    $pdf->Cell(40, 10, $pdf_row['item_price'], 1);
     $pdf->Cell(25, 10, $pdf_row['quantity'], 1);
     $pdf->Cell(30, 10, $pdf_row['payment_method'], 1);
     $pdf->Cell(35, 10, $code, 1);
     $pdf->Ln();
-    $tot += ( $pdf_row['quantity'] * $pdf_row['item_price']); // tính toán tổng giá trị của cột total
+    
+    $tot += ($pdf_row['quantity'] * $pdf_row['item_price']); // tính toán tổng giá trị của cột total
     $stt++;
 }
-    
-    // Tính toán các giá trị thống kê
-    $vat = $tot * 0.1; // tính tiền VAT (thuế giá trị gia tăng)
-    $grand_tot = $tot + $vat; // tính tổng tiền thanh toán
-    
-    // Xuất thông tin thống kê
-    $pdf->SetFont('Arial', 'B', 14);
-    $pdf->Ln();
-    $pdf->Cell(180,10,'THONG TIN THANH TOAN',1,1,'C');
-    $pdf->SetFont('Arial', 'B', 12);
-    $pdf->Cell(100,10,'Tong gia tri',1);
-    $pdf->Cell(80,10,number_format($tot).',000 VND',1,1,'R');
-    $pdf->Cell(100,10,'VAT (10%)',1);
-    $pdf->Cell(80,10,number_format($vat).',000 VND',1,1,'R');
-    $pdf->Cell(100,10,'Tong thanh toan',1);
-    $pdf->Cell(80,10,number_format($grand_tot).',000 VND',1,1,'R');
+
+// Tính toán các giá trị thống kê
+$vat = $tot * 0.1; // tính tiền VAT (thuế giá trị gia tăng)
+$grand_tot = $tot + $vat; // tính tổng tiền thanh toán
+
+// Xuất thông tin thống kê
+$pdf->SetFont('Arial', 'B', 14);
+$pdf->Ln();
+$pdf->Cell(180,10,'THONG TIN THANH TOAN',1,1,'C');
+$pdf->SetFont('Arial', 'B', 12);
+$pdf->Cell(100,10,'Tong gia tri',1);
+$pdf->Cell(80,10,number_format($tot).',000 VND',1,1,'R');
+$pdf->Cell(100,10,'VAT (10%)',1);
+$pdf->Cell(80,10,number_format($vat).',000 VND',1,1,'R');
+$pdf->Cell(100,10,'Tong thanh toan',1);
+$pdf->Cell(80,10,number_format($grand_tot).',000 VND',1,1,'R');
 
 $pdf->Ln();
-// Ghi file PDF và hiển thị cho người dùng
 $pdf->Cell(55,10,'Nguoi Nhap Kho: Nguyen Van A');
 $pdf->Ln();
 $pdf->Cell(55,10,'Nguoi Xuat Kho: Cong ty TNHH ABC');
